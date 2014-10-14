e7a801e8bf00dcb9122bb526b6e0db951fa23794
zhangchenghang <chenghang1937@126.com>
2014-10-14 09:35:08 +0800

    新增项

---
+++ ShoppingStore.Common/Auxiliary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Common
+{
+    public class Auxiliary
+    {
+        /// <summary>
+        /// MD5加密字符串
+        /// </summary>
+        /// <param name="encodingstr">待加密的字符串</param>
+        /// <returns>加密后的字符串</returns>
+        public static string MD5Encoding(string encodingstr)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] bs = Encoding.UTF8.GetBytes(encodingstr);
+            byte[] hs = md5.ComputeHash(bs);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hs)
+            {  
+                // 以十六进制格式格式化  
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();  
+        }
+        /// <summary>
+        /// MD5加盐值加密
+        /// </summary>
+        /// <param name="encodingstr">待加密的字符串</param>
+        /// <param name="saltvalue">盐值</param>
+        /// <returns>加密后的字符串</returns>
+        public static string MD5WithSaltEncoding(string encodingstr, object saltvalue)
+        {
+            if (saltvalue == null)
+                return encodingstr;
+            return MD5Encoding(encodingstr + "{" + saltvalue.ToString() + "}");
+        }
+    }
+}
