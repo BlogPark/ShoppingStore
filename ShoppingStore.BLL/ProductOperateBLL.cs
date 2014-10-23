aa48b07f036feb5e877eb8a2a9de69a0d98d6125
chenghang1937@hotmail.com <zhang1989>
2014-10-23 22:04:05 +0800

    添加产品处理类

---
+++ ShoppingStore.BLL/ProductOperateBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using ShoppingStore.DAL;
+using ShoppingStore.Model;
+
+namespace ShoppingStore.BLL
+{
+    /// <summary>
+    /// 产品操作逻辑类
+    /// </summary>
+    public class ProductOperateBLL
+    {
+
+    }
+}
---
+++ ShoppingStore.DAL/ProductOperateDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using ShoppingStore.Model;
+
+namespace ShoppingStore.DAL
+{
+    /// <summary>
+    /// 产品操作类
+    /// </summary>
+     public class ProductOperateDAL
+    {
+    }
+}
