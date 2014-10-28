031f6b259300d38141eafa647a861a2a3efe5150
zhangchenghang <chenghang1937@126.com>
2014-10-28 16:01:03 +0800

    完善和添加商品明细页面

---
+++ ShoppingStore.BLL/CategoriesOperateBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using ShoppingStore.Model;
+using ShoppingStore.DAL;
+
+namespace ShoppingStore.BLL
+{
+    public  class CategoriesOperateBLL
+    {
+        CategoriesOperateDAL dal = new CategoriesOperateDAL();
+        /// <summary>
+        /// 得到所有的主类类别
+        /// </summary>
+        /// <returns></returns>
+        public List<CategoriesModel> GetAllMainCategories()
+        {
+            List<CategoriesModel> categories = new List<CategoriesModel>();
+            DataTable dt = dal.GetAllMainCategories();
+            foreach (DataRow item in dt.Rows)
+            {
+                CategoriesModel model = new CategoriesModel();
+                model.cateid=int.Parse(item["cateid"].ToString());
+                model.name = item["name"].ToString();
+                categories.Add(model);
+            }
+            return categories;
+        }
+    }
+}
---
+++ ShoppingStore.Web/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingStore.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult itemdetail(int productid=1)
+        {
+            return View();
+        }
+
+    }
+}
