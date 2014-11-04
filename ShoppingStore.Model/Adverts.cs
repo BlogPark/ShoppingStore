cabda3d29e02536a000ead7244c3f3ab8daba323
zhangchenghang <chenghang1937@126.com>
2014-11-04 09:26:42 +0800

    tianjia类

---
+++ ShoppingStore.Model/Adverts.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ShoppingStore.Model
+{
+    [DataContract]
+    public class Adverts 
+    {
+        /// <summary>
+        /// adid
+        /// </summary>
+        [DataMember]
+        public int adid { get; set; }
+        /// <summary>
+        /// clickcount
+        /// </summary>
+        [DataMember]
+        public int clickcount { get; set; }
+        /// <summary>
+        /// adposid
+        /// </summary>
+        [DataMember]
+        public int adposid { get; set; }
+        /// <summary>
+        /// state
+        /// </summary>
+        [DataMember]
+        public int state { get; set; }
+        /// <summary>
+        /// starttime
+        /// </summary>
+        [DataMember]
+        public DateTime starttime { get; set; }
+        /// <summary>
+        /// endtime
+        /// </summary>
+        [DataMember]
+        public DateTime endtime { get; set; }
+        /// <summary>
+        /// displayorder
+        /// </summary>
+        [DataMember]
+        public int displayorder { get; set; }
+        /// <summary>
+        /// type
+        /// </summary>
+        [DataMember]
+        public int type { get; set; }
+        /// <summary>
+        /// title
+        /// </summary>
+        [DataMember]
+        public string title { get; set; }
+        /// <summary>
+        /// url
+        /// </summary>
+        [DataMember]
+        public string url { get; set; }
+        /// <summary>
+        /// body
+        /// </summary>
+        [DataMember]
+        public string body { get; set; }
+
+    }
+}
---
+++ ShoppingStore.Model/CategoriesPageModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ShoppingStore.Model
+{
+    /// <summary>
+    /// 商品分类列表页面的Model
+    /// </summary>
+    public class CategoriesPageModel
+    { 
+        /// <summary>
+        /// 分类信息
+        /// </summary>
+        public List<CategoriesModel> categories;
+        /// <summary>
+        /// 品牌信息
+        /// </summary>
+        public List<BrandsModel> brands;
+        /// <summary>
+        /// 热门品牌信息
+        /// </summary>
+        public List<BrandsModel> hotbrands;
+        /// <summary>
+        /// 商品信息
+        /// </summary>
+        public DataTable Products;
+        /// <summary>
+        /// 轮播图信息
+        /// </summary>
+        public List<Adverts> CarouselAdvertising;
+        /// <summary>
+        /// 热卖商品信息
+        /// </summary>
+        public List<ProductsModel> HotProducts;
+    }
+}
